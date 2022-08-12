61d411819352b71c0bc8827593da613ec9328c09
tanvir <tanvirulislam149@gmail.com>
2022-08-12 23:09:32 +0600

    cors issue check

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-   res.send("Warehouse management");
+   res.send("Warehouse management from heroku");
 });
 
 app.listen(port, () => {
